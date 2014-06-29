f1c56446a14e215e19c3f44e99e497ebb11021b6
Ruben Ascencio <rubena@amco.me>
2014-06-28 19:30:31 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -29,19 +29,16 @@ static const int GridColumns = 10;
 
 - (void)setupGrid
 {
-    // divide the grid's size by the number of columns/rows to figure out the right width and height of each cell
     _cellWidth = self.contentSize.width / GridColumns;
     _cellHeight = self.contentSize.height / GridRows;
 
     float x = 0;
     float y = 0;
 
-    // initialize the array as a blank NSMutableArray
     _gridArray = [NSMutableArray array];
 
-    // initialize Creatures
     for (int i = 0; i < GridRows; i++) {
-        // this is how you create two dimensional arrays in Objective-C. You put arrays into arrays.
+
         _gridArray[i] = [NSMutableArray array];
         x = 0;
 
@@ -51,15 +48,13 @@ static const int GridColumns = 10;
             creature.position = ccp(x, y);
             [self addChild:creature];
 
-            // this is shorthand to access an array inside an array
             _gridArray[i][j] = creature;
 
-            // make creatures visible to test this method, remove this once we know we have filled the grid properly
             creature.isAlive = YES;
-            
-            x+=_cellWidth;
+
+            x += _cellWidth;
         }
-        
+
         y += _cellHeight;
     }
 }
